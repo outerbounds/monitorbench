3a7f01fd66e6a3e9625bec9eb6d68a8f7d8b5785
Ville Tuulos <tuulos@gmail.com>
2023-12-20 11:48:54 -0800

    add more memory testing

--- monitorbench.py
+++ monitorbench.py
@@ -1,5 +1,8 @@
 import time
-from metaflow import FlowSpec, step, resources, parallel_map, Parameter
+import mmap
+from tempfile import NamedTemporaryFile
+
+from metaflow import FlowSpec, step, resources, parallel_map, Parameter, pypi_base
 
 
 def load_libc():
@@ -25,6 +28,22 @@ def spin_cpu(secs, half_load=False):
             time.sleep(0.0000002)
 
 
+def _make_file(name, size_in_mb):
+    x = b"1" * 1_000_000
+    with open(name, "wb") as f:
+        for i in range(size_in_mb):
+            f.write(x)
+
+
+def _print_mem():
+    import psutil
+
+    mem = psutil.Process().memory_info()
+    MB = 1024**2
+    print(f"Memory (MB): Resident {mem.rss // MB} Virtual {mem.vms // MB}")
+
+
+@pypi_base(python="3.11.0", packages={"psutil": "5.9.7"})
 class MonitorBench(FlowSpec):
     spin_secs = Parameter(
         "step_time", help="Run each step for this many seconds", default=120
@@ -107,6 +126,9 @@ class MonitorBench(FlowSpec):
             self.mem_staircase_8gb,
             self.mem_increasing_rss,
             self.mem_spike_2gb,
+            self.mem_mmap_8gb_untouch,
+            self.mem_mmap_8gb_touch_incrementally,
+            self.mem_mmap_8gb_touch_all,
         )
 
     @resources(memory=3000)
@@ -116,6 +138,7 @@ class MonitorBench(FlowSpec):
         Allocate 2GB and sit on it
         """
         t = b"a" * 2_000_000_000
+        _print_mem()
         time.sleep(self.spin_secs)
         self.next(self.mem_join)
 
@@ -126,6 +149,7 @@ class MonitorBench(FlowSpec):
         Allocate 8GB and sit on it
         """
         t = b"a" * 8_000_000_000
+        _print_mem()
         time.sleep(self.spin_secs)
         self.next(self.mem_join)
 
@@ -138,6 +162,7 @@ class MonitorBench(FlowSpec):
         t = []
         for i in range(16):
             t.append(b"a" * 512_000_000)
+            _print_mem()
             time.sleep(self.spin_secs / 17)
         time.sleep(self.spin_secs / 17)
         self.next(self.mem_join)
@@ -148,11 +173,11 @@ class MonitorBench(FlowSpec):
         """
         Make a 2GB allocation and fill it
         """
-        import mmap
-
         m = mmap.mmap(-1, 2_000_000_000, flags=mmap.MAP_PRIVATE)
         for i in range(2_000_000_000):
             m[i] = 66
+            if not i % 10_000_000:
+                _print_mem()
         time.sleep(10)
         m.close()
         self.next(self.mem_join)
@@ -167,9 +192,63 @@ class MonitorBench(FlowSpec):
         c = load_libc()
         p = c.malloc(2_000_000_000)
         c.memset(p, 66, 2_000_000_000)
+        print("spike!")
+        _print_mem()
         time.sleep(10)
         c.free(p)
+        print("after spike")
+        _print_mem()
         time.sleep(self.spin_secs / 2)
+        self.next(self.mem_join)
+
+    @resources(memory=2000)
+    @step
+    def mem_mmap_8gb_untouch(self):
+        """
+        Mmap a 8gb file without accessing it
+        """
+        with NamedTemporaryFile() as tmp:
+            _make_file(tmp.name, 8000)
+            with open(tmp.name, "r+b") as f:
+                mm = mmap.mmap(f.fileno(), 0, flags=mmap.ACCESS_READ)
+                _print_mem()
+                time.sleep(self.spin_secs / 2)
+        self.next(self.mem_join)
+
+    @resources(memory=10000)
+    @step
+    def mem_mmap_8gb_touch_incrementally(self):
+        """
+        Mmap a 8gb file and access it in 1GB increments
+        """
+        num_gigs = 8
+        with NamedTemporaryFile() as tmp:
+            _make_file(tmp.name, num_gigs * 1000)
+            with open(tmp.name, "r+b") as f:
+                r = 0
+                B = int(1e9)
+                mm = mmap.mmap(f.fileno(), 0, flags=mmap.ACCESS_READ)
+                for i in range(num_gigs):
+                    b = str(mm[i * B : (i + 1) * B])
+                    _print_mem()
+                    time.sleep(1)
+        self.next(self.mem_join)
+
+    @resources(memory=32000)
+    @step
+    def mem_mmap_8gb_touch_all(self):
+        """
+        Mmap a 8gb file and access it at once
+        """
+        num_gigs = 8
+        with NamedTemporaryFile() as tmp:
+            _make_file(tmp.name, num_gigs * 1000)
+            with open(tmp.name, "r+b") as f:
+                mm = mmap.mmap(f.fileno(), 0, flags=mmap.ACCESS_READ)
+                x = str(mm[:])
+                _print_mem()
+                time.sleep(self.spin_secs / 2)
+        x[0]
         self.next(self.mem_join)
 
     @step
@@ -188,10 +267,9 @@ class MonitorBench(FlowSpec):
         """
         time.sleep(self.spin_secs / 2)
         x = b"1" * 1_000_000_000
-        with open("testfile", "wb") as f:
-            for i in range(8):
-                f.write(x)
-                f.flush()
+        num_gigs = 8
+        with NamedTemporaryFile() as tmp:
+            _make_file(tmp.name, num_gigs * 1000)
         time.sleep(self.spin_secs / 2)
         self.next(self.io_join)
 
@@ -203,10 +281,9 @@ class MonitorBench(FlowSpec):
         """
         x = b"1" * 1_000_000_000
         for _ in range(10):
-            with open("testfile", "wb") as f:
-                for _ in range(8):
-                    f.write(x)
-                    f.flush()
+            num_gigs = 8
+            with NamedTemporaryFile() as tmp:
+                _make_file(tmp.name, num_gigs * 1000)
             spin_cpu(self.spin_secs / 10)
         self.next(self.io_join)
 
